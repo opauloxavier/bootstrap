81326da3bae687a9f9beb79900f27ad9cbbd6b75
unuzual <contato.pauloxavier@gmail.com>
2015-01-11 02:31:39 -0200

    bootstraping

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+	$name = $_POST['name'];
+	$age = $_POST['age'];
+	$password = $_POST['password'];
+	$email = $_POST['email'];
+
+	echo $name;
+	echo $age;
+	echo $password;
+	echo $email;
+?>
